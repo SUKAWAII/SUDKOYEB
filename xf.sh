1a0150f7552b4d8d18b691b1a0c37ac42a1f7081
SUKAWAII <102569405+SUKAWAII@users.noreply.github.com>
2022-03-29 11:22:49 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "de580d0b-e4bc-45c2-92da-ad357427afcf",
                         "alterId": 0
                     }
                 ],
